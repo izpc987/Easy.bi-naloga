e89b845554bcd99f219c5436589a7d40341e009b
izpc987 <37712211+izpc987@users.noreply.github.com>
2018-03-23 20:53:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var app = express();
+var mysql = require("mysql");
+var bodyParser = require("body-parser");
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "details"
+});
+
+app.listen(3000);
+app.set("view engine", "ejs");
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+app.post("/json", jsonParser, function(req, res) {
+
+    var name = req.body.name;
+    var lastname = req.body.lname;
+    var email = req.body.email;
+    
+    var message = {
+        status: 200,
+        success: "It worked"
+    }
+    
+    con.query("INSERT INTO user (name, lastname, email) VALUES (?, ?, ?)", [name, lastname, email], function(err) {
+        if (err) throw err;
+        message.status = 0;
+    });
+    
+    res.end(JSON.stringify(message));
+    
+});
+
+app.get("/", function(req, res) {
+    con.query("SELECT * FROM user", function(err, rows) {
+        if (err) throw err;
+        res.render("index", {
+           users: rows
+        });
+    });
+    
+});
